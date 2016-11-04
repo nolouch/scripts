02b99cfdb847291629d70aa5d904b7dd55e4ff75
cwen0 <cwenyin0@gmail.com>
2016-11-04 10:49:51 +0800

    update tidb start script

--- tidb/pd.sh
+++ tidb/pd.sh
@@ -1,10 +1,18 @@
-./bin/pd-server --cluster-id=1 \
-                --name=pd1 \
-                --data-dir=/data/pd  \
-                --client-urls="http://192.168.199.141:2379" \
-                --peer-urls="http://192.168.199.141:2380" \
-                --initial-cluster="pd1=http://192.168.199.141:2380,pd2=http://192.168.199.142:2380,pd3=http://192.168.199.143:2380"  \
-		-log-file /tmp/pd-server.log  \
-        --config pd_metric.toml & 
+#!/bin/bash
+ulimit -n 40960
+
+HOST=10.9.10.121
+NAME=pd1
+PD1_HOST=10.9.10.121
+PD2_HOST=10.9.184.132
+PD3_HOST=10.9.149.79
 
 
+cd /data/deploy && exec bin/pd-server \
+        --cluster-id=1 \
+        --data-dir=pd \
+        --config=conf/pd.toml \
+        --log-file=logs/pd.log \
+        --client-urls="http://${HOST}:2379" --peer-urls="http://${HOST}:2380" \
+        --name="${NAME}" \
+        --initial-cluster="pd1=http://${PD1_HOST}:2380,pd2=http://${PD2_HOST}:2380,pd3=http://${PD3_HOST}:2380"
--- tidb/tikv.sh
+++ tidb/tikv.sh
@@ -1,1 +1,16 @@
-./bin/tikv-server  -I 1 -S raftkv  --pd="192.168.199.141:2379,192.168.199.142:2379,192.168.199.143:2379" --addr="192.168.199.141:20160" -C tikv_config.toml --store=/data/tikv --log-file /tmp/tikv-server.log    &
+#!/bin/bash
+ulimit -n 4096
+
+export HOST=10.9.10.121
+
+export PD1_HOST=10.9.10.121
+export PD2_HOST=10.9.184.132
+export PD3_HOST=10.9.149.79
+
+export METRICS_HOST=10.9.10.121
+
+# unused: for listen on 0.0.0.0
+# --host="${HOST}"
+cd /data/deploy & exec bin/tidb-server -L=info --store=tikv -P 5000 --path="${PD1_HOST}:2379,${PD2_HOST}:2379,${PD3_HOST}:2379?cluster=1" \
+    --binlog-socket=status/pump.sock \
+    -lease 10 --status=10080 --log-file=logs/tidb.log --metrics-addr="${METRICS_HOST}:9091" --metrics-interval 15
