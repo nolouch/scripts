06c22091a1709180f90b67d334e0e998ccd31642
cwen0 <cwenyin0@gmail.com>
2016-11-17 12:33:49 +0800

    add test

---
+++ huawei/import_auto_increment.sh
@@ -0,0 +1,14 @@
+if  [ $# != 2 ];  then
+    echo "Usage: $0 port count"
+    exit 1
+fi
+
+port=$1
+#passwd=$2
+count=$2
+
+importer -t "create table t(a int primary key auto_increment, b double, c varchar(10));"  \
+       -i "create unique index u_b on t(b);"  \
+       -h  127.0.0.1  -P ${port} -D test   \
+       -n ${count}  \
+       -c 100 -b 100
