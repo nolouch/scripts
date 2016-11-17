e2e7d54e157ea0b522e4470dbe5b3c721e56891a
cwen0 <cwenyin0@gmail.com>
2016-11-17 14:27:23 +0800

    add create_indert.sh

---
+++ huawei/import1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if  [ $# != 2 ];  then
+    echo "Usage: $0 port count"
+    exit 1
+fi
+
+port=$1
+#passwd=$2
+count=$2
+
+importer -t "create table t(a int not null unique comment '[[step=1]]', b double, c varchar(10));"  \
+       -h  127.0.0.1  -P ${port} -D test   \
+       -n ${count}  \
+       -c 100 -b 100
