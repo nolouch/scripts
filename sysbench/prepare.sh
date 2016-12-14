d7e192459d3a1485728f61575235734f25661556
cwen0 <cwenyin0@gmail.com>
2016-12-14 19:52:08 +0800

    update test tidb

---
+++ sysbench/prepare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ulimit -n 40960
+
+PORT=$1
+DATABASE=$2
+SIZE=$3
+TABLES=$4
+TIDB_IMPORTER=$GOPATH/bin/importer
+echo $TIDB_IMPORTER
+
+
+for id in $(seq 1 $TABLES); do
+    echo creating sbtest$id
+    echo "create table if not exists sbtest$id(id INTEGER UNSIGNED primary key, k INTEGER , c varchar(120), pad CHAR(60));" | mysql -u root -P ${PORT} -h "$MYSQL_HOST" -D "${DATABASE}"
+    echo "create index k_$id on sbtest$id(k);" | mysql -u root -P ${PORT} -h "${MYSQL_HOST}" -D "${DATABASE}"
+done
+
+echo now set tidb lease != 0
+read
+
+for id in $(seq 1 $TABLES); do
+    echo insert sbtest$id ...
+    $TIDB_IMPORTER -u root -P ${PORT} -h "$MYSQL_HOST" -c 1280 -n SIZE -b 100 -D "${DATABASE}" \
+        -t "create table if not exists sbtest$id(id INTEGER UNSIGNED unique primary key comment '[[step=1]]', k INTEGER, c varchar(120), pad CHAR(60));"
+done
--- sysbench/test_tidb.sh
+++ sysbench/test_tidb.sh
@@ -1,58 +1,68 @@
 #!/bin/bash
 export MYSQL_HOST=127.0.0.1
-port=30040
+port=4000
+database="test"
+
 # test select
-echo "select 16*1000000 64"
-./prepare_read_only.sh ${port} "" 1000000 64 1200 16 && ./run_read_only.sh ${port} "" 1000000 64 1200 16 && ./reset1.sh
-echo "select 16*1000000 128"
-./prepare_read_only.sh ${port} "" 1000000 128 1200 16 && ./run_read_only.sh ${port} "" 1000000 128 1200 16 && ./reset1.sh
-echo "select 16*1000000 256"
-./prepare_read_only.sh ${port} "" 1000000 256 1200 16 && ./run_read_only.sh ${port} "" 1000000 256 1200 16 && ./reset1.sh
-echo "select 64*1000000 64"
-./prepare_read_only.sh ${port} "" 1000000 64 1200 64 && ./run_read_only.sh ${port} "" 1000000 64 1200 64 && ./reset1.sh
+echo "select 32*1000000 128"
+./prepare.sh ${port} ${database} 1000000  32
+./run_read_only.sh ${port} "" 1000000 128 1200 32
+sleep 60
+echo "select 32*1000000 256"
+./run_read_only.sh ${port} "" 1000000 256 1200 32
+sleep 60
+echo "select 32*1000000 612"
+./run_read_only.sh ${port} "" 1000000 612 1200 32
+./reset1.sh
 echo "select 64*1000000 128"
-./prepare_read_only.sh ${port} "" 1000000 128 1200 64 && ./run_read_only.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64
+./run_read_only.sh ${port} "" 1000000 128 1200 64
+sleep 60
 echo "select 64*1000000 256"
-./prepare_read_only.sh ${port} "" 1000000 256 1200 64 && ./run_read_only.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+./run_read_only.sh ${port} "" 1000000 256 1200 64
+sleep 60
+echo "select 64*1000000 612"
+./run_read_only.sh ${port} "" 1000000 612 1200 64
+./reset1.sh
 
 # test insert
-echo "insert 16*1000000 64"
-./prepare_insert.sh ${port} "" 1000000 64 1200 16 && ./run_insert.sh ${port} "" 1000000 64 1200 16 && ./reset1.sh
-echo "insert 16*1000000 128"
-./prepare_insert.sh ${port} "" 1000000 128 1200 16 && ./run_insert.sh ${port} "" 1000000 128 1200 16 && ./reset1.sh
-echo "insert 16*1000000 256"
-./prepare_insert.sh ${port} "" 1000000 256 1200 16 && ./run_insert.sh ${port} "" 1000000 256 1200 16 && ./reset1.sh
-echo "insert 64*1000000 64"
-./prepare_insert.sh ${port} "" 1000000 64 1200 64 && ./run_insert.sh ${port} "" 1000000 64 1200 64 && ./reset1.sh
+echo "insert 32*1000000 128"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_insert.sh ${port} "" 1000000 128 1200 32 && ./reset1.sh
+echo "insert 32*1000000 256"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_insert.sh ${port} "" 1000000 256 1200 32 && ./reset1.sh
+echo "insert 32*1000000 612"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_insert.sh ${port} "" 1000000 612 1200 32 && ./reset1.sh
 echo "insert 64*1000000 128"
-./prepare_insert.sh ${port} "" 1000000 128 1200 64 && ./run_insert.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_insert.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
 echo "insert 64*1000000 256"
-./prepare_insert.sh ${port} "" 1000000 256 1200 64 && ./run_insert.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_insert.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+echo "insert 64*1000000 612"
+./prepare.sh ${port} ${database} 1000000  64 && ./run_insert.sh ${port} "" 1000000 612 1200 64 && ./reset1.sh
 
 # test read_write
-echo "read_write 16*1000000 64"
-./prepare_read_write.sh ${port} "" 1000000 64 1200 16 && ./run_read_write.sh ${port} "" 1000000 64 1200 16 && ./reset1.sh
-echo "read_write 16*1000000 128"
-./prepare_read_write.sh ${port} "" 1000000 128 1200 16 && ./run_read_write.sh ${port} "" 1000000 128 1200 16 && ./reset1.sh
-echo "read_write 16*1000000 256"
-./prepare_read_write.sh ${port} "" 1000000 256 1200 16 && ./run_read_write.sh ${port} "" 1000000 256 1200 16 && ./reset1.sh
-echo "read_write 64*1000000 64"
-./prepare_read_write.sh ${port} "" 1000000 64 1200 64 && ./run_read_write.sh ${port} "" 1000000 64 1200 64 && ./reset1.sh
+echo "read_write 32*1000000 128"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_read_write.sh ${port} "" 1000000 128 1200 32 && ./reset1.sh
+echo "read_write 32*1000000 256"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_read_write.sh ${port} "" 1000000 256 1200 32 && ./reset1.sh
+echo "read_write 32*1000000 612"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_read_write.sh ${port} "" 1000000 612 1200 32 && ./reset1.sh
 echo "read_write 64*1000000 128"
-./prepare_read_write.sh ${port} "" 1000000 128 1200 64 && ./run_read_write.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_read_write.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
 echo "read_write 64*1000000 256"
-./prepare_read_write.sh ${port} "" 1000000 256 1200 64 && ./run_read_write.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_read_write.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+echo "read_write 64*1000000 612"
+./prepare.sh ${port} ${database} 1000000  64 && ./run_read_write.sh ${port} "" 1000000 612 1200 64 && ./reset1.sh
 
 # test update
-echo "update 16*1000000 64"
-./prepare_update.sh ${port} "" 1000000 64 1200 16 && ./run_update.sh ${port} "" 1000000 64 1200 16 && ./reset1.sh
-echo "update 16*1000000 128"
-./prepare_update.sh ${port} "" 1000000 128 1200 16 && ./run_update.sh ${port} "" 1000000 128 1200 16 && ./reset1.sh
-echo "update 16*1000000 256"
-./prepare_update.sh ${port} "" 1000000 256 1200 16 && ./run_update.sh ${port} "" 1000000 256 1200 16 && ./reset1.sh
-echo "update 64*1000000 64"
-./prepare_update.sh ${port} "" 1000000 64 1200 64 && ./run_update.sh ${port} "" 1000000 64 1200 64 && ./reset1.sh
+echo "update 32*1000000 128"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_update.sh ${port} "" 1000000 128 1200 32 && ./reset1.sh
+echo "update 32*1000000 256"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_update.sh ${port} "" 1000000 256 1200 32 && ./reset1.sh
+echo "update 32*1000000 512"
+./prepare.sh ${port} ${database} 1000000  32 && ./run_update.sh ${port} "" 1000000 612 1200 32 && ./reset1.sh
 echo "update 64*1000000 128"
-./prepare_update.sh ${port} "" 1000000 128 1200 64 && ./run_update.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_update.sh ${port} "" 1000000 128 1200 64 && ./reset1.sh
 echo "update 64*1000000 256"
-./prepare_update.sh ${port} "" 1000000 256 1200 64 && ./run_update.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+./prepare.sh ${port} ${database} 1000000  64 && ./run_update.sh ${port} "" 1000000 256 1200 64 && ./reset1.sh
+echo "update 64*1000000 612"
+./prepare.sh ${port} ${database} 1000000  64 && ./run_update.sh ${port} "" 1000000 612 1200 64 && ./reset1.sh
